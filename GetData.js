7bcbc5e88c1d5ba079fb1f3d06628e6d753a3dfd
gdhashmi <dastagir85@gmail.com>
2019-11-04 23:43:02 +0100

    update

--- GetData.js
+++ GetData.js
@@ -9,9 +9,10 @@ const fetchOptions = {
  async function fetchData() {
     const response = await fetch('https://cors-anywhere.herokuapp.com/https://api.cuescore.com/tournament/?id=6647635', fetchOptions);
     lData = await response.json();
-    //console.log('Received response..');
-    //console.log(JSON.stringify(Data));
+    console.log('Received response..');
+    console.log(Object.keys(lData));
     return 1;
 }
+console.log(Object.keys(lData));
 //fetch('https://cors-anywhere.herokuapp.com/https://api.cuescore.com/tournament/?id=6647635', fetchOptions).then((res) => {console.log('Waiting for response'); res.json()}).then((data) => {console.log('Processing response' + JSON.stringify(data)); getTournamentDetails(data)});
 fetchData().then(getTournamentDetails(lData));
