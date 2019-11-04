3af64040c82ca2a0434c8384809da2d070a49b74
gdhashmi <dastagir85@gmail.com>
2019-11-04 23:40:25 +0100

    update

--- GetData.js
+++ GetData.js
@@ -1,17 +1,17 @@
 import {getTournamentDetails } from './analyzeData.js';
-var Data;
+var lData;
 
 const fetchOptions = { 
     method: 'GET',
     origin:'https://gdhashmi.github.io/', 
  }
 
- //async function fetchData(Data) {
-   // const response = await fetch('https://cors-anywhere.herokuapp.com/https://api.cuescore.com/tournament/?id=6647635', fetchOptions);
-    //Data = await response.json();
+ async function fetchData() {
+    const response = await fetch('https://cors-anywhere.herokuapp.com/https://api.cuescore.com/tournament/?id=6647635', fetchOptions);
+    lData = await response.json();
     //console.log('Received response..');
     //console.log(JSON.stringify(Data));
-    //return 1;
-//}
-fetch('https://cors-anywhere.herokuapp.com/https://api.cuescore.com/tournament/?id=6647635', fetchOptions).then((res) => {console.log('Waiting for response'); res.json()}).then((data) => {console.log('Processing response' + JSON.stringify(data)); getTournamentDetails(data)});
-//fetchData(Data).then(getTournamentDetails(Data));
+    return 1;
+}
+//fetch('https://cors-anywhere.herokuapp.com/https://api.cuescore.com/tournament/?id=6647635', fetchOptions).then((res) => {console.log('Waiting for response'); res.json()}).then((data) => {console.log('Processing response' + JSON.stringify(data)); getTournamentDetails(data)});
+fetchData().then(getTournamentDetails(lData));
