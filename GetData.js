37b744bdd0c41425e1c591f2a2980972c8b2d8f5
gdhashmi <dastagir85@gmail.com>
2019-11-04 23:10:26 +0100

    update

--- GetData.js
+++ GetData.js
@@ -1,6 +1,4 @@
 import {getTournamentDetails } from './analyzeData.js';
-const app = document.getElementById('root')
-app.textContent = 'Fetching Data..';
 const fetchOptions = { 
     method: 'GET',
     origin:'https://gdhashmi.github.io/', 
@@ -8,9 +6,9 @@ const fetchOptions = {
 async function fetchData() {
     const response = await fetch('https://cors-anywhere.herokuapp.com/https://api.cuescore.com/tournament/?id=6647635', fetchOptions);
     console.log(response)
-    const myJson = await response.json()
+    const myJson = await response.json();
+    console.log('Received response..');
     console.log(JSON.stringify(myJson));
-    app.textContent = JSON.stringify(myJson);
     return myJson;
 }
 var lData = fetchData();
