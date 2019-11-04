cab856ee05c14ed8ce121d26066c8b31652794bd
gdhashmi <49479801+gdhashmi@users.noreply.github.com>
2019-11-04 10:49:22 +0100

    Update GetData.js

--- GetData.js
+++ GetData.js
@@ -3,22 +3,21 @@ const app = document.getElementById('root')
 app.textContent = 'Fetching Data..';
 const fetchOptions = { 
     method: 'GET',
-    mode: 'no-cors',
+   // mode: 'no-cors',
     headers : {
         'Content-Type' : 'application/json',
-        "Accept": 'application/json',
-        //'From-Origin' : 'Localhost',
-        'Access-Control-Allow-Origin': '*',
-        credentials: 'same-origin'
+        Accept: 'application/json',
+        origin:'https://gdhashmi.github.io/', 
     },
-    "Accept": 'application/json',
+    origin:'https://gdhashmi.github.io/', 
+    Accept : 'application/json',
     'Content-Type' : 'application/json'
     //data: {
     //    id : 6647635 
     //}
 }
 async function fetchData() {
-    const response = await fetch('https://api.cuescore.com/tournament/?id=6647635', fetchOptions);
+    const response = await fetch('https://cors-anywhere.herokuapp.com/https://api.cuescore.com/tournament/?id=6647635', fetchOptions);
     console.log(response)
     const myJson = await response.json()
     console.log(JSON.stringify(myJson));
