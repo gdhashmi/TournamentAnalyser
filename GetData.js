94c3273fb85d00bf874d96b55ae2dba18943a3e0
gdhashmi <49479801+gdhashmi@users.noreply.github.com>
2019-11-05 17:22:12 +0100

    Update GetData.js

--- GetData.js
+++ GetData.js
@@ -1,20 +1,26 @@
 import {getTournamentDetails } from './analyzeData.js';
+var lData;
 
 const fetchOptions = { 
     method: 'GET',
     origin:'https://gdhashmi.github.io/', 
  }
 
-var lData =  async () => {
+ function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+  
+ async function fetchData() {
     const response = await fetch('https://cors-anywhere.herokuapp.com/https://api.cuescore.com/tournament/?id=6647635', fetchOptions);
-    await response.json();
+    await sleep(5000);
+    lData = await response.json();
     console.log('Received response..');
     console.log(JSON.stringify(lData));
-//    return 1;
+    return 1;
 }
+sleep(5000);
 console.log('Getting Keys');
 //console.log(Object.keys(lData));
 //fetch('https://cors-anywhere.herokuapp.com/https://api.cuescore.com/tournament/?id=6647635', fetchOptions).then((res) => {console.log('Waiting for response'); res.json()}).then((data) => {console.log('Processing response' + JSON.stringify(data)); getTournamentDetails(data)});
-//fetchData().then(sleep(5000).then(
- getTournamentDetails(lData);
-//));
+fetchData().then(sleep(5000).then(
+    getTournamentDetails(lData)));
